8dc5c5ccd6a21568822aa074c9dd45461266e70e
Remi Salmi <remi@MacBook-Pro-de-Remi.local>
2020-10-23 08:53:50 +0200

    init project

---
+++ src/main/java/com/polytech/alertcovidserviceuser/AlertCovidServiceUserApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidserviceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertCovidServiceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertCovidServiceUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/alertcovidserviceuser/AlertCovidServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidserviceuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertCovidServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
