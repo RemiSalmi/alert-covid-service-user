7814470b185c24adaa3ec6e023fedc4a05c9d799
Remi Salmi <remi.salmi@gmail.com>
2020-11-08 19:17:50 +0100

    fix cors

--- src/main/java/com/polytech/alertcovidserviceuser/AlertCovidServiceUserApplication.java
+++ src/main/java/com/polytech/alertcovidserviceuser/AlertCovidServiceUserApplication.java
@@ -2,12 +2,28 @@ package com.polytech.alertcovidserviceuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AlertCovidServiceUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlertCovidServiceUserApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
